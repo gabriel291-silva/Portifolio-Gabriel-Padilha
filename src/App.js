49c825988df24fdf2afde366150cf9e941ba383e
gabriel291-silva <sbiel20013@gmail.com>
2022-06-14 18:14:58 -0300

    feat/footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from './component/header/header.tsx';
+import { Route,Routes, BrowserRouter } from "react-router-dom";
+import Home from './component/home/home.tsx';
+import Footer from './component/footer/footer.tsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <BrowserRouter>
+       <Routes>
+       <Route  element={<Home/>}   path="/"  />
+       </Routes>
+       </BrowserRouter>
+       <Footer/>
     </div>
   );
 }
---
+++ src/component/footer/footer.tsx
@@ -0,0 +1,87 @@
+import "./footer.scss"
+function Footer(){
+    return(
+        <footer>
+            <div className="footer-contents">
+
+                
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                <div className="list-contents">
+                    <h3>title</h3>
+                    <ul className="category-list">
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                    <li className="category">teste</li>
+                </ul>
+                </div>
+                
+                
+
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
